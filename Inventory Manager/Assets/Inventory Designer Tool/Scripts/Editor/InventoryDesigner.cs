474891b8001a0572b5b52c4ce11151342d12909e
mvmijares <michaelv.mijares@gmail.com>
2019-02-22 19:30:18 -0500

    Added tutorial project

---
+++ Inventory Manager/Assets/Inventory Designer Tool/Scripts/Editor/InventoryDesigner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class InventoryDesigner : EditorWindow {
+    [MenuItem("Window/Inventory Designer")]
+    static void OpenWindow() {
+        InventoryDesigner window = (InventoryDesigner)GetWindow(typeof(InventoryDesigner));
+        window.minSize = new Vector2(600, 300);
+        window.Show();
+    }
+}
---
+++ Inventory Manager/Assets/Resources/characterData/Scripts/MageData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Types;
+
+public class MageData : CharacterData {
+    public MageDmgType dmgType;
+    public MageWpnType wpnType;
+
+}
